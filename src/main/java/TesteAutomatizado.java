313a73ec795ccb396c39e94644e48f6a71d17790
angel <angel@DESKTOP-27E4LQM>
2019-11-03 01:14:10 -0200

    Cap1 - inicio projeto

---
+++ src/main/java/TesteAutomatizado.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteAutomatizado {
+	public static void main(String[] args) {
+		//configura caminho absoluto
+//		System.setProperty("webdriver.gecko.driver", "C:\\Users\\angel\\Desktop\\wks_Photon\\Selenium_Alura\\bin\\geckodriver-v0.26.0-win32\\geckodriver.exe");
+		
+		//configura caminho relativo
+		System.setProperty("webdriver.gecko.driver", "bin/geckodriver-v0.26.0-win32/geckodriver.exe");
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.google.com.br");
+		
+		WebElement query = driver.findElement(By.name("q"));
+		query.sendKeys("Caelum");
+		
+		//submete o fom
+		query.submit();
+	}
+}
